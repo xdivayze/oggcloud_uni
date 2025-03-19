67a1b030b46318b07a8526d0010b95fbbf2a872a
xdivayze <ozangencay123@gmail.com>
2025-03-19 16:18:50 +0300

    branch init

---
+++ frontend/src/Routes/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../../Navbar/Navbar";
+
+export default function Profile() {
+  return (
+    <div className="w-full mx-7">
+      <div className="pt-14 min-h-1/5">
+        <Navbar collapse={true} />
+      </div>
+    </div>
+  );
+}
